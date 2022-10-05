e4e50830cca80799bc68c35046aebd81255ad3d1
Maruchi Kim <maruchi.kim@gmail.com>
2022-10-05 15:38:44 -0700

    More debug...

--- axelerate/networks/common_utils/convert.py
+++ axelerate/networks/common_utils/convert.py
@@ -89,7 +89,6 @@ class Converter(object):
         num_imgs = 300
         image_files_list = []
         from axelerate.networks.common_utils.feature import create_feature_extractor
-        print("test")
         backend = create_feature_extractor(self._backend, [self._img_size[0], self._img_size[1]])
         print("build feature extractor")
         image_search = lambda ext : glob.glob(self._dataset_path + ext, recursive=True)
@@ -105,6 +104,7 @@ class Converter(object):
             data = np.array(backend.normalize(image), dtype=np.float32)
             data = np.expand_dims(data, 0)
             bin_filename = os.path.basename(filename).split('.')[0]+'.bin'
+            print(bin_filename)
             with open(os.path.join(temp_folder, bin_filename), "wb") as f:
                 data = np.transpose(data, [0, 3, 1, 2])
                 data.tofile(f)
